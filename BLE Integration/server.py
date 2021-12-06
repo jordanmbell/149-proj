6544f6a4c09b3495c5fcb17487d40a38a82c26ce
Jordan Bell <headlock22066@gmail.com>
2021-12-05 18:33:39 -0800

    small communication changes

--- BLE Integration/server.py
+++ BLE Integration/server.py
@@ -22,8 +22,8 @@ marker_to_robot = {
 
 # Main server loop
 async def main():
-    shared_data = shared_data_t(4)
-    handle_sigint(None, shared_data)
+    shared_data, comm_tasks = await begin_communication(4)
+    handle_sigint(comm_tasks, shared_data)
 
     # For now, assume that everything is connected
     shared_data.disconnect = False
@@ -46,10 +46,15 @@ async def main():
         # Update shared date for BLE clients
         while not shared_data.disconnect:
             if shared_data.connected:
+                change = False
                 for i in range(4):
                     if i in marker_to_robot.keys():
-                        shared_data.update_robot(marker_to_robot[i],
-                         shared_dict[i][0], shared_dict[i][1], shared_dict[i][1])
+                        shared_data.update_robot(marker_to_robot[i], 
+                                shared_dict[i][0], shared_dict[i][1], shared_dict[i][1])
+                        change = True
+                if change:
+                    shared_data.push_update()
+
             await asyncio.sleep(0.1)
 
         # Clean up
@@ -58,6 +63,11 @@ async def main():
         print("Waiting to close camera")
         p_track.join()
         print("Finished cleanup")
+    # Main loop finished, await communication shutdown
+    print("Shutdown communication")
+    shared_data.disconnect = True
+    await comm_tasks
+    print("All tasks finished")
 
 if __name__=="__main__":
     asyncio.run(main())
