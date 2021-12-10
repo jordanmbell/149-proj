67649193e1ff7f75b936fcb362932c252f0ab8f8
Jordan Bell <jordanmbell@berkeley.edu>
2021-12-10 13:32:15 -0800

    fixup

--- buckler/software/apps/final_project/controller.c
+++ buckler/software/apps/final_project/controller.c
@@ -630,7 +630,7 @@ robot_state_t controller(robot_state_t state) {
           lsm9ds1_measurement_t meas = lsm9ds1_read_gyro_integration();
           current_ang = meas.z_axis + current_ang;
 
-          float ideal_speed = (set_distance_or_angle + initial_angle - current_ang ) / ((LOC_TIME[counter] + enter_state_time) - current_time);
+          float ideal_speed = ((set_distance_or_angle / 180 * M_PI) + initial_angle - current_ang ) / ((LOC_TIME[counter] + enter_state_time) - current_time);
           drive_formatted(0, ideal_speed);
           printf("speed: %d, angular: %f\n", 0, ideal_speed);
           
@@ -689,7 +689,7 @@ robot_state_t controller(robot_state_t state) {
           lsm9ds1_measurement_t meas = lsm9ds1_read_gyro_integration();
           current_ang = meas.z_axis + current_ang;
 
-          float ideal_speed = (set_distance_or_angle - initial_angle + current_ang) / ((LOC_TIME[counter] + enter_state_time) - current_time);
+          float ideal_speed = ((set_distance_or_angle / 180 * M_PI) - initial_angle + current_ang) / ((LOC_TIME[counter] + enter_state_time) - current_time);
           drive_formatted(0, -ideal_speed);
           snprintf(buf, 16, "%.2f, %.2f, %.2f", set_distance_or_angle, initial_angle, current_ang);
           display_write(buf, DISPLAY_LINE_1);
