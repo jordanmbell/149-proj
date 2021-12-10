9c68c3c05de47914b7376a1c79082a0c106371d5
Jordan Bell <jordanmbell@berkeley.edu>
2021-12-10 15:01:39 -0800

    fixup

--- buckler/software/apps/final_project/controller.c
+++ buckler/software/apps/final_project/controller.c
@@ -615,7 +615,7 @@ robot_state_t controller(robot_state_t state) {
           radd = sqrt(pow(rad + initial_location_x, 2) + pow(initial_location_y, 2));
           current_x = my_position->x_pos;
           current_y = my_position->y_pos;
-          get_relative_xy(&relative_x, &relative_y, counter - 1, modified_r_mat, current_time - enter_state_time, spd, -1, current_x, current_y, init_state_x, init_state_y, &end_x, &end_y);
+          get_relative_xy(&relative_x, &relative_y, counter - 1, modified_r_mat, current_time - enter_state_time, spd, radd, current_x, current_y, init_state_x, init_state_y, &end_x, &end_y);
           printf("x %f, y %f, inx %f, iny %f,rx %f, ry %f \n", current_x, current_y, init_state_x, init_state_y, relative_x, relative_y);
           // printf("t: %f \n",current_time);
           d1 = relative_y - d1;
@@ -671,7 +671,7 @@ robot_state_t controller(robot_state_t state) {
           radd = sqrt(pow(rad - initial_location_x, 2) + pow(initial_location_y, 2));
           current_x = my_position->x_pos;
           current_y = my_position->y_pos;
-          get_relative_xy(&relative_x, &relative_y, counter - 1, modified_r_mat, current_time - enter_state_time, spd, -1, current_x, current_y, init_state_x, init_state_y, &end_x, &end_y);
+          get_relative_xy(&relative_x, &relative_y, counter - 1, modified_r_mat, current_time - enter_state_time, spd, radd, current_x, current_y, init_state_x, init_state_y, &end_x, &end_y);
           printf("x %f, y %f, inx %f, iny %f,rx %f, ry %f \n", current_x, current_y, init_state_x, init_state_y, relative_x, relative_y);
           d1 = relative_y - d1;
           d2 = relative_x - d2;
