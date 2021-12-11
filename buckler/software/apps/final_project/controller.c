691843819e8eb2761804d362e627f3feb78f49f9
Jordan Bell <jordanmbell@berkeley.edu>
2021-12-11 10:58:59 -0800

    fixup

--- buckler/software/apps/final_project/controller.c
+++ buckler/software/apps/final_project/controller.c
@@ -52,7 +52,7 @@ char buf[16];
 static simple_ble_config_t ble_config = {
     // c0:98:e5:49:xx:xx
     .platform_id = 0x49,     // used as 4th octect in device BLE address
-    .device_id = 0x0870,     // TODO: replace with your lab bench number
+    .device_id = 0x9870,     // TODO: replace with your lab bench number
     .adv_name = "EE149 LED", // used in advertisements if there is room
     .adv_interval = MSEC_TO_UNITS(1000, UNIT_0_625_MS),
     .min_conn_interval = MSEC_TO_UNITS(10, UNIT_1_25_MS),
