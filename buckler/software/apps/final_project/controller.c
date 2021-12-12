41c19dc819329cecb57b06086be2ea62cb493c94
Jordan Bell <jordanmbell@berkeley.edu>
2021-12-12 14:46:30 -0800

    fixup

--- buckler/software/apps/final_project/controller.c
+++ buckler/software/apps/final_project/controller.c
@@ -663,12 +663,12 @@ robot_state_t controller(robot_state_t state) {
         {
           turning_in_place = true;
           lsm9ds1_measurement_t meas = lsm9ds1_read_gyro_integration();
-          double comp_1 = fabs(meas.z_axis * M_PI / 180) +
-                          fabs(last_global_angle - initial_angle);
-          double comp_2 = set_distance_or_angle * M_PI / 180;
+          current_ang = meas.z_axis * M_PI / 180 + last_global_angle;
+          double comp_1 = current_ang;
+          double comp_2 = set_distance_or_angle * M_PI / 180 + initial_angle;
           double delta_ang = atan2(sin(comp_1 - comp_2), cos(comp_1 - comp_2));
           printf("Delta ang: %f\n", delta_ang);
-          if (delta_ang <= 0) {
+          if (delta_ang >= 0) {
             // double ideal_speed = set_distance_or_angle / 180 * M_PI / time_constant;
             drive_formatted(0, IN_PLACE_SPEED);
             // printf("ideal_speed: %f, set_angle: %f, initial_angle: %f, current_angle: %f, task_time: %f, enter_time: %f, current_time: %f\n", ideal_speed, set_distance_or_angle, initial_angle, current_ang, LOC_TIME[counter - 1], enter_state_time, current_time);
@@ -731,9 +731,9 @@ robot_state_t controller(robot_state_t state) {
         {
           turning_in_place = true;
           lsm9ds1_measurement_t meas = lsm9ds1_read_gyro_integration();
-          double comp_1 = fabs(meas.z_axis * M_PI / 180) +
-                          fabs(last_global_angle - initial_angle);
-          double comp_2 = set_distance_or_angle * M_PI / 180;
+          current_ang = meas.z_axis * M_PI / 180 + last_global_angle;
+          double comp_1 = current_ang;
+          double comp_2 = -1 * set_distance_or_angle * M_PI / 180 + initial_angle;
           double delta_ang = atan2(sin(comp_1 - comp_2), cos(comp_1 - comp_2));
           if (delta_ang <= 0) {
             // double ideal_speed = set_distance_or_angle / 180 * M_PI / time_constant;
