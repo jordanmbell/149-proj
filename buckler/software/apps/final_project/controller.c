6ff15e99e4d5d6cabf32c6c323d5636b23d65ecc
Jordan Bell <jordanmbell@berkeley.edu>
2021-12-12 14:59:44 -0800

    fixup

--- buckler/software/apps/final_project/controller.c
+++ buckler/software/apps/final_project/controller.c
@@ -654,7 +654,7 @@ robot_state_t controller(robot_state_t state) {
           i1 += relative_y;
           i2 += relative_x;
           drive_formatted(velocity - relative_y * Kp1 + d1 * Kd1 + i1 * Ki1, velocity / radd / 1000 + Kp2 * relative_x + d2 * Kd2 + i2 * Ki2);
-          snprintf(buf, 16, "%.2f %.2f %.2f", relative_x, relative_y, rel_angle);
+          snprintf(buf, 16, "%.2f %.2f %.2f", relative_x, relative_y, should_angle);
           display_write(buf, DISPLAY_LINE_0);
           snprintf(buf, 16, "%.2f %.2f %.2f", current_x, current_y, current_ang);
           display_write(buf, DISPLAY_LINE_1);
